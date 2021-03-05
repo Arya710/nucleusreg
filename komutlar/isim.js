d178061e48d985b820882eb62d9f313505e8551f
Arya710 <56167941+Arya710@users.noreply.github.com>
2021-03-06 01:00:13 +0300

    Add files via upload

---
+++ komutlar/isim.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const db = require('quick.db');
+exports.run = async (client, message, args) => {
+  
+ if(!message.member.roles.cache.has('814424825048334357')) return message.channel.send('**Bu komutu kullanabilmek için \`Confirmation Staff\` yetkisine sahip olmasınız.**')
+
+  let member = message.mentions.members.first();
+  let isim = args[1]
+  let yaş = args[2]
+ 
+  if (!member) return message.channel.send("Bir Kullanıcı Etiketle");
+  if (!isim) return message.channel.send("Bir İsim Girmelisin!");
+  if(!yaş) return message.channel.send("Yaş Girmelısın.")
+
+  
+  db.add(`nick_${message.author.id}`,1) 
+  message.channel.send("İsim Başarıyla Değiştirildi!")
+  member.setNickname(`${isim} ' ${yaş}`);
+  
+    
+  
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: 0
+}
+exports.help = {
+  name: 'nick',
+} 
